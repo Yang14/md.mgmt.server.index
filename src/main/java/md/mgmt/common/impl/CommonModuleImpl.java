2a6154a3777ef5dac45b1adb6070dde4a11068d3
Yang14 <359097729@qq.com>
2016-01-13 10:48:49 +0800

    impl commonModule

--- src/main/java/md/mgmt/common/impl/CommonModuleImpl.java
+++ src/main/java/md/mgmt/common/impl/CommonModuleImpl.java
@@ -4,6 +4,7 @@ import md.mgmt.base.md.ClusterNodeInfo;
 import md.mgmt.common.CommonModule;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
@@ -30,18 +31,37 @@ public class CommonModuleImpl implements CommonModule {
 
     @Override
     public ClusterNodeInfo getMdLocation(Integer distrCode) {
-        if (distrCode % 1 ==0)
-        return null;
+        if (distrCode == null) {
+            return null;
+        }
+        ClusterNodeInfo clusterNodeInfo = new ClusterNodeInfo();
+        if (distrCode % 3 == 0) {
+            clusterNodeInfo.setIp("node-03");
+        } else if (distrCode % 2 == 0) {
+            clusterNodeInfo.setIp("node-02");
+        } else {
+            clusterNodeInfo.setIp("node-01");
+        }
+        clusterNodeInfo.setDistrCode(distrCode);
+        clusterNodeInfo.setPort(8008);
+        return clusterNodeInfo;
     }
 
 
     @Override
     public ClusterNodeInfo genMdLocation() {
-        return null;
+        return getMdLocation(genDistrCode());
     }
 
     @Override
     public List<ClusterNodeInfo> getMdLocationList(List<Integer> distrCodeList) {
-        return null;
+        if (distrCodeList == null) {
+            return null;
+        }
+        List<ClusterNodeInfo> nodeInfos = new ArrayList<ClusterNodeInfo>();
+        for (int code : distrCodeList) {
+            nodeInfos.add(getMdLocation(code));
+        }
+        return nodeInfos;
     }
 }
