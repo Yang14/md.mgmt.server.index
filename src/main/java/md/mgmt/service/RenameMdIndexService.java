0039b4108555c6f410bb16ce8c9bbd0c7c91b8c0
Yang14 <359097729@qq.com>
2016-01-16 18:46:52 +0800

    add rename func. & test case

---
+++ src/main/java/md/mgmt/service/RenameMdIndexService.java
@@ -0,0 +1,21 @@
+package md.mgmt.service;
+
+import md.mgmt.base.md.MdIndex;
+import md.mgmt.facade.resp.find.DirMdAttrPosList;
+import md.mgmt.facade.resp.find.FileMdAttrPosList;
+
+/**
+ * Created by Mr-yang on 16-1-12.
+ */
+public interface RenameMdIndexService {
+
+    /**
+     * 1.获取父目录索引
+     * 2.获取文件索引
+     * 3.组合成文件存在的范围编码
+     * 4.公共模块得到所有节点地址
+     */
+    public FileMdAttrPosList findFileMdIndex(MdIndex mdIndex);
+
+    public DirMdAttrPosList findDirMdIndex(MdIndex mdIndex);
+}
---
+++ src/main/java/md/mgmt/service/impl/RenameMdIndexServiceImpl.java
@@ -0,0 +1,7 @@
+package md.mgmt.service.impl;
+
+/**
+ * Created by Mr-yang on 16-1-16.
+ */
+public class RenameMdIndexServiceImpl {
+}
